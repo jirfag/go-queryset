2754c6bde1e6ba43c5bdc16aae9c2476987de5e8
stffabi <stffabi@pm.me>
2018-11-27 07:38:50 +0100

    Use default GOPATH on go1.8+ if GOPATH is not defined

---
+++ parser/go18.go
@@ -0,0 +1,9 @@
+// +build go1.8
+
+package parser
+
+import (
+	"go/build"
+)
+
+var defaultGOPATH = build.Default.GOPATH
---
+++ parser/not_go18.go
@@ -0,0 +1,5 @@
+// +build !go1.8
+
+package parser
+
+var defaultGOPATH = ""
